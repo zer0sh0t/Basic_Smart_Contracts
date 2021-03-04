b0aae396fae60f6cdfc79c27c5c85f9dfd6127b1
Harin Kumar <65289179+zer0sh0t@users.noreply.github.com>
2021-03-05 00:09:28 +0530

    first commit

---
+++ Greeter.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+contract Greeter  {
+    string public yourName;
+    
+   function Greeter() public {
+        yourName = "nothing";
+    } 
+    
+    function set(string name)public {
+        yourName = name;
+    }
+    
+    function hello() constant public returns (string) {
+        return yourName;
+    }
+}
---
+++ SimpleStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.0; 
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() constant public returns (uint)  {
+        return storedData;
+    }
+    
+    function increment (uint n) public {
+        storedData = storedData + n;
+        return;
+    }
+    
+    function decrement (uint n) public {
+        storedData = storedData - n;
+        return;
+    }
+}
+
+ 
+
+
